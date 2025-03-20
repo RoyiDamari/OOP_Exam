9c5e1079dd43533a43dfcde0562e18d7757eb789
RoyiDamari <royi66.damari@gmail.com>
2025-03-20 19:38:55 +0200

    Initial commit

---
+++ exercise_1.py
@@ -0,0 +1,74 @@
+# question 1
+# answer b
+
+# question 2
+# answer c
+
+# question 3
+# answer b
+
+# question 4
+# answer c
+
+# question 5
+# answer b
+
+# question 6
+# answer a
+
+# question 7
+# answer b
+
+# question 8
+# answer b
+
+# question 9
+# answer c
+
+# question 10
+# answer b
+
+# question 11
+# answer b
+
+# question 12
+# answer a
+
+# question 13
+# answer b
+
+# question 14
+# answer a
+
+# question 15
+# answer b
+
+# question 16
+# answer b
+
+# question 17
+# answer d
+
+# question 18
+# answer b
+
+# question 19
+# answer b
+
+# question 20
+# answer b
+
+# question 21
+# answer b
+
+# question 22
+# answer b
+
+# question 23
+# answer b
+
+# question 24
+# answer b
+
+# question 25
+# answer c
---
+++ exercise_2.py
@@ -0,0 +1,44 @@
+# question 1
+# answer a
+
+# question 2
+# answer a
+
+# question 3
+# answer c
+
+# question 4
+# answer a
+
+# question 5
+# answer a
+
+# question 6
+# answer b
+
+# question 7
+# answer a
+
+# question 8
+# answer b
+
+# question 9
+# answer b
+
+# question 10
+# answer b
+
+# question 11
+# answer c
+
+# question 12
+# answer a
+
+# question 13
+# answer a
+
+# question 14
+# answer b
+
+# question 15
+# answer b
